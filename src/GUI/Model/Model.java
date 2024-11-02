29943c06b15be53f7f66c9f4334080e74fad7333
Jakub969 <jakub.hrubizna@gmail.com>
2024-11-02 11:44:13 +0100

    pridavanie nehnutelnosti v modely

--- src/GUI/Model/Model.java
+++ src/GUI/Model/Model.java
@@ -16,12 +16,17 @@ public class Model {
     private KdStrom<Nehnutelnost> kdStromNehnutelnosti;
     private KdStrom<Parcela> kdStromParciel;
     private KdStrom<GeografickyObjekt> kdStromGeografickychObjektov;
+    OperaciaGeografickychObjektov<Nehnutelnost> operacieNehnutelnosti;
+    OperaciaGeografickychObjektov<Parcela> operacieParciel;
+    OperaciaGeografickychObjektov<GeografickyObjekt> operacieGeografickychObjektov;
 
     public Model() {
         this.kdStromNehnutelnosti = new KdStrom<>(2);
-        OperaciaGeografickychObjektov<Nehnutelnost> operacieNehnutelnosti = new OperaciaGeografickychObjektov<>(this.kdStromNehnutelnosti);
+        this.operacieNehnutelnosti = new OperaciaGeografickychObjektov<>(this.kdStromNehnutelnosti);
         this.kdStromParciel = new KdStrom<>(2);
-        OperaciaGeografickychObjektov<Parcela> operacieParciel = new OperaciaGeografickychObjektov<>(this.kdStromParciel);
+        this.operacieParciel = new OperaciaGeografickychObjektov<>(this.kdStromParciel);
+        this.kdStromGeografickychObjektov = new KdStrom<>(2);
+        this.operacieGeografickychObjektov = new OperaciaGeografickychObjektov<>(this.kdStromGeografickychObjektov);
     }
 
     public List<Nehnutelnost> searchNehnutelnosti(double dlzka, double sirka) {
@@ -49,9 +54,40 @@ public class Model {
 
     public void addNehnutelnost(int supisneCislo, String popis, List<GPS> gpsPositions) {
         // Modify Nehnutelnost to store all GPS positions if needed
-        Nehnutelnost newNehnutelnost = new Nehnutelnost(supisneCislo, popis, null, null, gpsPositions.get(0));
+        Nehnutelnost newNehnutelnost1 = new Nehnutelnost(supisneCislo, popis, null, null, gpsPositions.getFirst());
+        Nehnutelnost newNehnutelnost2 = new Nehnutelnost(supisneCislo, popis, null, null, gpsPositions.getLast());
+        ArrayList<Vrchol<Nehnutelnost>> vysledok = operacieNehnutelnosti.metodaVkladania(newNehnutelnost1, newNehnutelnost2);
+        GeografickyObjekt newGeografickyObjekt1 = new GeografickyObjekt(gpsPositions.getFirst(), vysledok.getFirst().getData(), null);
+        GeografickyObjekt newGeografickyObjekt2 = new GeografickyObjekt(gpsPositions.getLast(), vysledok.getLast().getData(), null);
+        operacieGeografickychObjektov.metodaVkladania(newGeografickyObjekt1, newGeografickyObjekt2);
+    }
 
-        Vrchol<Nehnutelnost> newVrchol = new Vrchol<>(newNehnutelnost);
-        kdStromNehnutelnosti.vloz(newVrchol);
+    public void addParcela(int cisloParcely, String popis, List<GPS> gpsPositions) {
+        Parcela newParcela = new Parcela(cisloParcely, popis, null, null, gpsPositions.get(0));
+
+        Vrchol<Parcela> newVrchol = new Vrchol<>(newParcela);
+        kdStromParciel.vloz(newVrchol);
+    }
+
+    public List<Parcela>  searchParcela(double dlzka, double sirka) {
+        // Create a temporary `Parcela` for comparison purposes
+        GPS searchGPS = new GPS('N', sirka, 'E', dlzka);
+        Parcela searchParcela = new Parcela(0, "", null, null, searchGPS);
+
+        // Perform the KD-tree search based on the initial GPS position
+        Vrchol<Parcela> vrcholVyhladavania = new Vrchol<>(searchParcela);
+        ArrayList<Vrchol<Parcela>> kluce = new ArrayList<>();
+        kluce.add(vrcholVyhladavania);
+        ArrayList<Vrchol<Parcela>> results = kdStromParciel.bodoveVyhladavanie(kluce);
+
+        // Filter results by using the `porovnaj` method to check if they match within tolerance
+        return results.stream()
+                .map(Vrchol::getData)
+                .filter(parcela -> {
+                    // Use `porovnaj` for both longitude (poradieKluca 0) and latitude (poradieKluca 1)
+                    return searchParcela.porovnaj(parcela, 0) == 0 &&
+                            searchParcela.porovnaj(parcela, 1) == 0;
+                })
+                .collect(Collectors.toList());
     }
 }
--- src/testy/OperaciaGeografickychObjektov.java
+++ src/testy/OperaciaGeografickychObjektov.java
@@ -3,8 +3,11 @@ package testy;
 import US.KdStrom.KdStrom;
 import US.KdStrom.Vrchol;
 import rozhrania.IKluc;
+import triedy.GeografickyObjekt;
 import triedy.Nehnutelnost;
 import triedy.Parcela;
+
+import java.util.ArrayList;
 
 public class OperaciaGeografickychObjektov<T extends IKluc<T>> {
     private final KdStrom<T> strom;
@@ -13,7 +16,7 @@ public class OperaciaGeografickychObjektov<T extends IKluc<T>> {
         this.strom = kdStrom;
     }
 
-    public void metodaVkladania(T objekt1, T objekt2) {
+    public ArrayList<Vrchol<T>> metodaVkladania(T objekt1, T objekt2) {
         if (objekt1 instanceof Nehnutelnost dataNehnutelnost1 && objekt2 instanceof Nehnutelnost dataNehnutelnost2) {
             dataNehnutelnost1.setReferenciaNaRovnakuNehnutelnostSInymiGPS(dataNehnutelnost2);
             dataNehnutelnost2.setReferenciaNaRovnakuNehnutelnostSInymiGPS(dataNehnutelnost1);
@@ -21,6 +24,10 @@ public class OperaciaGeografickychObjektov<T extends IKluc<T>> {
             this.strom.vloz(vrchol1);
             Vrchol<T> vrchol2 = new Vrchol<>(objekt2);
             this.strom.vloz(vrchol2);
+            ArrayList<Vrchol<T>> vrcholy = new ArrayList<>();
+            vrcholy.add(vrchol1);
+            vrcholy.add(vrchol2);
+            return vrcholy;
         } else if (objekt1 instanceof Parcela dataParcela1 && objekt2 instanceof Parcela dataParcela2) {
             dataParcela1.setReferenciaNaRovnakuParceluSInymiGPS(dataParcela2);
             dataParcela2.setReferenciaNaRovnakuParceluSInymiGPS(dataParcela1);
@@ -28,6 +35,21 @@ public class OperaciaGeografickychObjektov<T extends IKluc<T>> {
             this.strom.vloz(vrchol1);
             Vrchol<T> vrchol2 = new Vrchol<>(objekt2);
             this.strom.vloz(vrchol2);
+            ArrayList<Vrchol<T>> vrcholy = new ArrayList<>();
+            vrcholy.add(vrchol1);
+            vrcholy.add(vrchol2);
+            return vrcholy;
+        } else if (objekt1 instanceof GeografickyObjekt dataGeografickyObjekt1 && objekt2 instanceof GeografickyObjekt dataGeografickyObjekt2) {
+            Vrchol<T> vrchol1 = new Vrchol<>(objekt1);
+            this.strom.vloz(vrchol1);
+            Vrchol<T> vrchol2 = new Vrchol<>(objekt2);
+            this.strom.vloz(vrchol2);
+            ArrayList<Vrchol<T>> vrcholy = new ArrayList<>();
+            vrcholy.add(vrchol1);
+            vrcholy.add(vrchol2);
+            return vrcholy;
+        } else {
+            return null;
         }
     }
 }
